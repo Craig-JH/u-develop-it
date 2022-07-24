b7e4dd3522efb9bff2b0a572fe95c8f2b745bff0
Craig Hyman <misccorp@yahoo.com>
2022-07-24 11:31:56 -0400

    added a candidate route to change their party

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 const express = require("express");
-const inputCheck = require('./utils/inputCheck');
+const inputCheck = require("./utils/inputCheck");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,7 +24,7 @@ const db = mysql.createConnection(
 
 // Get all candidates
 app.get("/api/candidates", (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
+  const sql = `SELECT candidates.*, parties.name 
     AS party_name 
     FROM candidates 
     LEFT JOIN parties 
@@ -44,7 +44,7 @@ app.get("/api/candidates", (req, res) => {
 
 // Get a single candidate
 app.get("/api/candidate/:id", (req, res) => {
-    const sql = `SELECT candidates.*, parties.name 
+  const sql = `SELECT candidates.*, parties.name 
     AS party_name 
     FROM candidates 
     LEFT JOIN parties 
@@ -52,6 +52,64 @@ app.get("/api/candidate/:id", (req, res) => {
     WHERE candidates.id = ?`;
   const params = [req.params.id];
 
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// Update a candidate's party
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE candidates SET party_id = ? 
+                 WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Candidate not found",
+      });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
+app.get("/api/parties", (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+app.get("/api/party/:id", (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
   db.query(sql, params, (err, row) => {
     if (err) {
       res.status(400).json({ error: err.message });
@@ -86,6 +144,27 @@ app.delete("/api/candidate/:id", (req, res) => {
   });
 });
 
+app.delete("/api/party/:id", (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Party not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
 // Create a candidate
 app.post("/api/candidate", ({ body }, res) => {
   const errors = inputCheck(
@@ -100,18 +179,18 @@ app.post("/api/candidate", ({ body }, res) => {
   }
   const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
   VALUES (?,?,?)`;
-const params = [body.first_name, body.last_name, body.industry_connected];
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    res.status(400).json({ error: err.message });
-    return;
-  }
-  res.json({
-    message: 'success',
-    data: body
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
   });
-});
 });
 
 // Create a candidate
